b19725cdbb85fdf11a31d1375abe46a9a5319272
bernabaris <bernabaris@marun.edu.tr>
2023-07-12 15:50:24 +0300

    solution added

--- 11.container-with-most-water.java
+++ 11.container-with-most-water.java
@@ -14,8 +14,7 @@ class Solution {
             int l1 = Math.min(height[start], height[end]);
             int l2 = end - start;
             int area =  l1 * l2;
-
-
+            
             maxArea = Math.max(maxArea, area);
             // Pointerlar kontrol edilir
             if (height[start] < height[end]) {
