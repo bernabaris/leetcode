fdbffd99593dd5ba03619b084490ff46ec1b424b
bernabaris <bernabaris@marun.edu.tr>
2023-08-01 17:12:15 +0300

    605 problem solved

---
+++ 605.can-place-flowers.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=605 lang=java
+ *
+ * [605] Can Place Flowers
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        
+    }
+}
+// @lc code=end
+
